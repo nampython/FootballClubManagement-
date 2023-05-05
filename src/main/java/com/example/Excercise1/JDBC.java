694dfd952e918cfbb7e44de04d4c0e6a8e171520
NamQuang13 <dragonsangsang10a1@gmail.com>
2023-05-05 14:59:54 +0700

    Message = test locking the branch master

--- src/main/java/com/example/Excercise1/JDBC.java
+++ src/main/java/com/example/Excercise1/JDBC.java
@@ -16,22 +16,5 @@ public class JDBC {
 
 
     public static void main(String[] args) {
-// Open a connection
-//        try {
-//            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-//            Statement stmt = conn.createStatement();
-//            ResultSet rs = stmt.executeQuery(QUERY);
-//            // Extract data from result set
-//            while (rs.next()) {
-//                // Retrieve by column name
-//                System.out.println("STADIUM: " + rs.getString("stadium"));
-//                System.out.println("ABOUT: " + rs.getString("about"));
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-
-        // init map
-
     }
 }
