a3eb13b3e4075eea4fb4c70bce52cf9716abef73
nam.dinh <dragonsang10a1@gmail.com>
2023-05-09 10:36:48 +0700

    #11 - Update Cart

--- src/main/java/com/example/Excercise1/Cart.java
+++ src/main/java/com/example/Excercise1/Cart.java
@@ -2,6 +2,6 @@ package com.example.Excercise1;
 
 public class Cart {
     public void addCart() {
-        System.out.println("Adding cart");
+        System.out.println("Updated Adding cart");
     }
 }
